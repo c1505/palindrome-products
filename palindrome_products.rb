d7fd5e4b07c6fd2c6ce83bd868d96ef81b1338db
c1505 <coreym247@gmail.com>
2017-02-13 18:55:18 -0500

    first commit

---
+++ palindrome_products.rb
@@ -0,0 +1,17 @@
+class Palindromes
+  attr_reader :value
+  def initialize(args)
+    @value = value
+  end
+
+  def generate
+  end
+
+  def largest
+    self
+  end
+
+  # def value
+  #
+  # end
+end
