b0ffb7bf8bc965491bdd9e847b04f108118eb96c
c1505 <coreym247@gmail.com>
2017-02-13 19:10:32 -0500

    first test passing

--- palindrome_products.rb
+++ palindrome_products.rb
@@ -1,7 +1,7 @@
 class Palindromes
   attr_reader :value
   def initialize(args)
-    @value = value
+    @value = args[:max_factor]
   end
 
   def generate
@@ -11,6 +11,26 @@ class Palindromes
     self
   end
 
+  def factors
+    @factors_array = []
+    @factors_array << [1, self.value]
+    factor1 = factor[0]
+    factor2 = self.value / factor1
+    @factors_array << [factor1, factor2]
+    # array of arrays
+  end
+
+  def factor
+    divisor = 2
+    factors = []
+    while divisor < self.value
+      if self.value % divisor == 0
+        factors << divisor
+      end
+      divisor += 1
+    end
+    factors
+  end
   # def value
   #
   # end
